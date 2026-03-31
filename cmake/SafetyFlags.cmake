4781e80042e6443d37b5339e0ecd577d90d83f19
Armin Felder <armin.felder@gmail.com>
2026-03-31 23:50:27 +0200

    chore: add comment

--- cmake/SafetyFlags.cmake
+++ cmake/SafetyFlags.cmake
@@ -13,6 +13,9 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# based on https://airbus-seclab.github.io/c-compiler-security/
+# adds stricter flags like -Wunsafe-buffer-usage
+
 include(CheckCXXCompilerFlag)
 
 # === Warning Configuration ===
